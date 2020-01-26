a638e3278f9312a9b538470cbfbda621a0c08506
Jonathan Orozco <socaljorozco@gmail.com>
2020-01-25 20:44:28 -0800

    initial commit of report card assessment for proguide

---
+++ data/data.js
@@ -0,0 +1,22 @@
+export const students = [
+  {
+    "courseId": 1,
+    "courseName": "Biology"
+  },
+  {
+    "courseId": 2,
+    "courseName": "Math"
+  },
+  {
+    "courseId": 3,
+    "courseName": "History"
+  },
+  {
+    "courseId": 4,
+    "courseName": "English"
+  },
+  {
+    "courseId": 5,
+    "courseName": "Psychology"
+  }
+]
